e022797b5362ba4f1c0a93659cdf24c07398ae21
github username <github email address>
2019-01-21 15:35:35 -0500

    add files to repo

---
+++ src/index.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+addHeader()
+getAllVideos()
+
+})
+
+function body(){
+  return document.body
+}
+
+function addHeader(){
+  h1 = document.createElement('h1')
+  h1.innerText = "Flatiron School LearnTube 1.0"
+  document.body.appendChild(h1)
+}
+
+function getAllVideos(){
+  fetch('http://localhost:3000/api/v1/videos')
+  .then(r => r.json())
+  .then(videos => {
+    videos.forEach(video => renderVideo(video))
+  })
+}
+
+function renderVideo(video){
+  vidCard = document.createElement('div')
+  vidCard.id = `vid-card-${video.id}`
+  vidCard.classList.add('vid-card')
+  
+    vidName = document.createElement('h3')
+    vidName.innerText = video.name
+    vidCard.appendChild(vidName)
+
+    vidFrame = document.createElement('iframe')
+    vidFrame.src = video.url
+    vidCard.appendChild(vidFrame)
+  body().appendChild(vidCard)
+}
